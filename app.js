52e811e4a074edf3525e889f55c9b5e60f02d540
pachi <aybanh@gmail.com>
2013-04-08 14:43:21 +1000

    portfix

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var express = require('express')
   , io = require('socket.io').listen(server, {'log level': 2})
   , game = require('./app/controllers/game');
 
+var port = process.env.PORT || 8000;
+
 app.configure(function(){
     app.use(express.static(__dirname + '/public'));
 });
 
-server.listen(8000);
+server.listen(port);
 
 io.sockets.on('connection', function (socket) {
 	game.create(socket);
